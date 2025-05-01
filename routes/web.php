de9a1829bc30a8cc38a777c42259a51aa7bed6fb
adejui <adejuinurrochim@email.com>
2025-05-01 15:43:48 +0700

    feat: complete all route definitions

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseVideoController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscribeTransactionController;
+use App\Http\Controllers\TeacherController;
+use App\Models\Teacher;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+Route::get('/details/{course:slug}', [FrontController::class, 'details'])->name('front.details');
+
+Route::get('/category/{category:slug}', [FrontController::class, 'category'])->name('front.category');
+
+Route::get('/pricing', [FrontController::class, 'pricing'])->name('front.pricing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +26,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // must logged in before create a transaction
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout');
+    Route::post('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout.store');
+
+    Route::prefix('admin')->name('admin')->group(function () {
+        Route::resource('categories', CategoryController::class)->middleware('role:owner'); // admin.categories.index
+
+        Route::resource('teachers', TeacherController::class)->middleware('role:owner');
+
+        Route::resource('courses', CourseController::class)->middleware('role:owner|teacher');
+
+        Route::resource('subscribe_transactions', SubscribeTransactionController::class)->middleware('role:owner');
+
+        Route::resource('course_videos', CourseVideoController::class)->middleware('role:owner|teacher');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
