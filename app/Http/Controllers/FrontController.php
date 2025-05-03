19180f32df0ebc36661cd31386342bc6d03f061b
adejui <adejuinurrochim@email.com>
2025-05-04 00:28:50 +0700

    feat: implement subscription checkout functionality

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreSubscribeTransactionRequest;
+use App\Models\SubscribeTransaction;
 
 class FrontController extends Controller
 {
@@ -43,5 +46,33 @@ class FrontController extends Controller
     public function checkout()
     {
         return view('front.checkout');
+    }
+
+    public function checkout_store(StoreSubscribeTransactionRequest $request)
+    {
+        $user = Auth::user();
+
+        if ($user->hasActiveSubscription()) {
+            return redirect()->route('front.index');
+        }
+
+        DB::transaction(function () use ($request, $user) {
+
+            $validated = $request->validated();
+
+            if ($request->hasFile('proof')) {
+                $proofPath = $request->file('proof')->store('proofs', 'public');
+                $validated['proof'] = $proofPath;
+            }
+
+            $validated['user_id'] = $user->id;
+            $validated['total_amount'] = 499000;
+            $validated['is_paid'] = false;
+
+
+            $transaction = SubscribeTransaction::create($validated);
+        });
+
+        return redirect()->route('dashboard');
     }
 }
